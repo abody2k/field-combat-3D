aab93525bad1a75f85ce4fe7f64495ec1fcd5084
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2023-10-14 10:29:20 +0300

    fixed rotation and movement of a tank

--- scenes/tank.gd
+++ scenes/tank.gd
@@ -1,26 +1,35 @@
 extends "res://scripts/AI.gd"
 
 
-const SPEED = 30
+const SPEED = 10
 ##the tank movement is different because it needs to go in a circular path if it wants to rotate
 @export var v : Vector3
 var firstDeg
 
 func _ready():
 	pass
-#	target = get_parent_node_3d().get_node("target")
+	firstDeg=rotation.z
+	target = get_parent_node_3d().get_node("target")
+	print(deg_to_rad(0))
 #	firstDeg= basis.z
 #	basis = Basis(Vector3.UP,deg_to_rad(190))
 
 
-
+var stop =false
 func move(delta : float =0):
-	basis = Basis(Vector3.UP,(lerp(0.0,position.angle_to(target.position),delta)))
 	
+	print(rad_to_deg(position.angle_to(target.position)))
+	if rad_to_deg(position.angle_to(target.position)) > 1 :
+		basis = Basis(Vector3.UP,(lerp(position.angle_to(target.position),0.0,delta*30)))
+	else :
+		stop = true
 	pass
-
+var x = 0.0
 @export var anglee : float
 func _physics_process(delta):
-
+	if not stop:
+		move(delta)
+	velocity = basis.x * SPEED
+	move_and_slide()
 #	$MeshInstance3D.basis = Basis(Vector3.UP,deg_to_rad(anglee))
 	pass
